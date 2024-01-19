51eb849e4ab37b535acf14f41da2e367ba099ba7
PatricioOsorio <patriciomiguel_12@hotmail.com>
2024-01-18 22:21:00 -0600

    Agregado sitio al autenticarse

--- TIdentity/wwwroot/js/site.js
+++ TIdentity/wwwroot/js/site.js
@@ -1,4 +1,61 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// ==================================
+// ASIDE
+// ==================================
+document.addEventListener('DOMContentLoaded', function (event) {
+  const showNavbar = (toggleId, navId, bodyId, headerId) => {
+    const toggle = document.getElementById(toggleId),
+      nav = document.getElementById(navId),
+      bodypd = document.getElementById(bodyId),
+      headerpd = document.getElementById(headerId);
 
-// Write your JavaScript code.
+    // Validate that all variables exist
+    if (toggle && nav && bodypd && headerpd) {
+      toggle.addEventListener('click', () => {
+        // show navbar
+        nav.classList.toggle('show');
+        // change icon
+        toggle.classList.toggle('bx-x');
+        // add padding to body
+        bodypd.classList.toggle('body-pd');
+        // add padding to header
+        headerpd.classList.toggle('body-pd');
+      });
+    }
+  };
+
+  showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header');
+
+  // Link active
+  const linkColor = document.querySelectorAll('.nav_link');
+
+  function colorLink() {
+    if (linkColor) {
+      linkColor.forEach((l) => l.classList.remove('active'));
+      this.classList.add('active');
+    }
+  }
+  linkColor.forEach((l) => l.addEventListener('click', colorLink));
+
+});
+
+// ==================================
+// SCROLL TOP
+// ==================================
+const $btnScrollTop = document.querySelector('.scrollTop');
+
+window.addEventListener('scroll', (e) => {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  let viewportHeight = window.innerHeight;
+
+  scrollTop > viewportHeight
+    ? $btnScrollTop.classList.remove('hidden')
+    : $btnScrollTop.classList.add('hidden');
+});
+$btnScrollTop.addEventListener('click', (e) => {
+  if (e.target.matches('.scrollTop') || e.target.matches(`.scrollTop *`)) {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
+});
---
+++ query.sql
@@ -0,0 +1,9 @@
+USE TIdentity;
+
+SELECT * FROM AspNetRoleClaims;
+SELECT * FROM AspNetRoles;
+SELECT * FROM AspNetUserClaims;
+SELECT * FROM AspNetUserLogins;
+SELECT * FROM AspNetUserRoles;
+SELECT * FROM AspNetUsers;
+SELECT * FROM AspNetUserTokens;
